fd2967b0949d8312c5f423b6eeb8ae2227b90340
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-10-12 18:13:02 +0300

    Выполнена основная верстка

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import {Route} from 'react-router-dom'
 
-const App = () => {
-  return <div className="App">Init</div>
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" component={Home} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  )
 }
 
 export default App
