80025f86e33e7b64311d702433c6dd3b966c1611
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-10-13 22:30:51 +0300

    Загрузка данных с бд, выбор пиццы и размера в ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import axios from 'axios'
+
 import Header from './components/Header'
+import {Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
-import {Route} from 'react-router-dom'
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+      setPizzas(data.pizzas)
+    })
+  }, [])
+  console.log(pizzas)
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
